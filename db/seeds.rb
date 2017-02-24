e07c61b5601c10cdddf89f244b0c860489bc19b4
danielmoralesp <daniel_moralesp@hotmail.com>
2017-02-24 11:45:52 -0500

    modelos, asociaciones, seed y pruebas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+=begin
+producto = Product.create(name: "Kindle", price: 120)
+producto2 = Product.create(name: "Toy Story", price: 45)
+
+## Usado en linea de comandos
+producto3 = Product.create(name: "Camisa XS", price: 35)
+
+category1 = Category.create(name: "Libros")
+category2 = Category.create(name: "Tecnologia")
+category3 = Category.create(name: "Electrodomesticos")
+category4 = Category.create(name: "Aseo")
+category5 = Category.create(name: "Juguetes")
+
+## Usado en linea de comandos
+category6 = Category.create(name: "Ropa")
+category7 = Category.create(name: "Accesorios")
+category8 = Category.create(name: "Remates")
+
+=end
+
+Category.create(name: 'Perfumes')
+Category.create(name: 'Libros')
+Category.create(name: 'Tecnologia')
+Category.create(name: 'Electrodomesticos')
+Category.create(name: 'Aseo')
+Category.create(name: 'Juguetes')
+Category.create(name: 'Ropa')
+Category.create(name: 'Oficina')
+Category.create(name: 'Mascotas')
